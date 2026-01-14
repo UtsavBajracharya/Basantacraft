f2602a1c222b4f975e99bd21e803b4b572e1d634
Utsav Bajracharya <utsavbajra619@gmail.com>
2026-01-14 00:38:20 -0500

    add tailwind styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-const Home = () => {
-  return <div>Home</div>;
+import React from 'react';
 
+const Home = () => {
+
+  return (
+    <div className="p-10 bg-light-pink">
+      <h2 className="text-xl font-semibold">Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Harum, nisi. Est, veniam quis quaerat deleniti aliquid 
+        perspiciatis delectus cum eligendi ducimus dolore commodi 
+        id temporibus iste ad quod explicabo similique!</p>
+    </div>
+  )
 };
 
 export default Home;
-
